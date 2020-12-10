dd24c0f62f2c9ecad52ebb06284f4bf8bdee5b3f
Flakes WRLD <71187213+FristyFlakes@users.noreply.github.com>
2020-12-10 12:45:05 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN","1419998054:AAG-sW4Yv7x3uhrWaRUpsHe6_W0Uay1Cl64")
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY","a71c791c-87ca-48ba-9534-3744916b0302")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN","")
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY","")
     AUTHORIZED_USERS = [int(user) for user in os.environ.get("AUTHORIZED_USERS").split(" ")]
     TG_CHARACTER_LIMIT = 4000 
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME","herokuflixbot")
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME","")
